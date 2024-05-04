e5d4d8cb084357ad54d9b1eecfe2695d271f751e
shymi001 <113885455+shymi001@users.noreply.github.com>
2024-05-05 04:08:44 +0500

    Add files via upload

---
+++ gogole/main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('6712478774:AAGLy-WkBix3w6NlIxcG2R51CQAoDMbslnw')
+dp = Dispatcher(bot) 
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(
+        types.KeyboardButton('Button 1', web_app=WebAppInfo(url='https://shymi001.github.io/TgBotNew/')))
+    await message.answer('Hello, my друг!', reply_markup=markup)
+
+executor.start_polling(dp)
+
